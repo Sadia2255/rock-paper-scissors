8f973f1e93e44d0a1edac7a327d0bc6b2c2077d2
Sadia Rahman <122247468+Sadia2255@users.noreply.github.com>
2024-08-11 17:50:20 -0400

    Add files via upload

---
+++ rock-paper-scissors.js
@@ -0,0 +1,97 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+function updateScore() {
+  document.querySelector('.js-score')
+    .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function totalGames() {
+  document.querySelector('.js-total-games')
+    .innerHTML = `Total Games: ${score.wins + score.losses + score.ties}`;
+}
+
+updateScore();
+totalGames();
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+  let computerMove = '';
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = 'Rock';
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = 'Paper';
+  } else {
+    computerMove = 'Scissors';
+  }
+
+  return computerMove;
+}
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+  let result = '';
+
+  if (playerMove === 'Rock') {
+    if (computerMove === 'Rock') {
+      result = 'Tie! 😌';
+    } else if (computerMove === 'Paper') {
+      result = 'You lose! 😓';
+    } else {
+      result = 'You win! 😁';
+    }
+  } else if (playerMove === 'Paper') {
+    if (computerMove === 'Rock') {
+      result = 'You win! 😁';
+    } else if (computerMove === 'Paper') {
+      result = 'Tie! 😌';
+    } else {
+      result = 'You lose! 😓';
+    }
+  } else {
+    if (computerMove === 'Rock') {
+      result = 'You lose! 😓';
+    } else if (computerMove === 'Paper') {
+      result = 'You win! 😁';
+    } else {
+      result = 'Tie! 😌';
+    }
+  }
+
+  if (result === 'You win! 😁') {
+    score.wins++;
+  } else if (result === 'You lose! 😓') {
+    score.losses++;
+  } else {
+    score.ties++;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));
+
+  updateScore();
+  totalGames();
+
+  document.querySelector('.js-player-move')
+    .innerHTML = `You picked: ${playerMove}.`;
+
+  document.querySelector('.js-computer-move')
+    .innerHTML = `Computer picked: ${computerMove}.`;
+
+  document.querySelector('.js-result')
+    .innerHTML = result;
+}
+
+function reset() {
+  document.querySelector('.js-player-move')
+    .innerHTML = '';
+
+  document.querySelector('.js-computer-move')
+    .innerHTML = '';
+
+  document.querySelector('.js-result')
+    .innerHTML = '';
+}
